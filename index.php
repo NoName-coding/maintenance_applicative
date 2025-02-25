525288b970dc3d929cc2cad1be4d5c1756c3b5aa
Sergent Lucas <lucas.sergent@etu.univ-littoral.fr>
2025-02-25 15:24:15 +0100

    Modification du css

--- index.php
+++ index.php
@@ -10,9 +10,6 @@
     <h1>Bienvenue sur notre site</h1>
     <?php if (isset($_SESSION['user'])): ?>
         <p>Vous êtes connecté en tant que <?php echo $_SESSION['user']; ?>.</p>
-        <a href="logout.php">Déconnexion</a>
-    <?php else: ?>
-        <a href="login.php">Se connecter</a>
     <?php endif; ?>
 </body>
 </html>
